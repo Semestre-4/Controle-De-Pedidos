1c7a671de6260fb193765ee822f5eeecbb64db76
Gustavo Piegat <gustavopiegat2004@gmail.com>
2023-07-30 12:42:24 -0300

    Adicionado menu de clientes em um metodo da classe ControleClientes

--- src/main/java/org/desafio/ControleClientes.java
+++ src/main/java/org/desafio/ControleClientes.java
@@ -22,6 +22,58 @@ public class ControleClientes {
 
     }
 
+    public  void  menuClientes(){
+        int escolha;
+
+
+        do{
+
+            String nome;
+            String cpf;
+            String telefone;
+
+
+            String rua;
+            int numero;
+            String complemento;
+            int verificaComplemento;
+
+            System.out.println("\n| ============================= |");
+            System.out.println("| ==== Cadastro de Cliente ==== | ");
+            System.out.println("| ============================= |");
+            System.out.println("|                               |");
+            System.out.println("| ===== Digite uma opção ====== |");
+            System.out.println("| ===== 1: Cadastrar Novo ===== |");
+            System.out.println("| ==== 2: Listar Clientes ===== |");
+            System.out.println("| ==== 3: Procurar Cliente ==== |");
+            System.out.println("| ========= 0: Voltar ========= |");
+            escolha = s.nextInt();
+
+            switch (escolha){
+
+
+                case 1:
+
+                    cadastrarNovoCliente();
+                    break;
+
+                case 2:
+
+                    listarClientes();
+                    break;
+
+                case 3:
+                    procurarCliente();
+                    break;
+
+                default:
+                    System.out.println("| ===== Opção invalida! ===== |");
+                    break;
+            }
+
+        }while(escolha != 0);
+    }
+
 
     public List<Clientes> getClientes() {
         return clientes;
--- src/main/java/org/desafio/Main.java
+++ src/main/java/org/desafio/Main.java
@@ -7,60 +7,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
-        ControleClientes controle = new ControleClientes();
-        int escolha;
+        ControleClientes controleClientes = new ControleClientes();
         Scanner s = new Scanner(System.in);
 
         List<Clientes> clientes = new ArrayList<>();
 
 
+        controleClientes.menuClientes();
 
-        do{
-
-            String nome;
-            String cpf;
-            String telefone;
-
-
-            String rua;
-            int numero;
-            String complemento;
-            int verificaComplemento;
-
-            System.out.println("\n| ============================= |");
-            System.out.println("| ==== Cadastro de Cliente ==== | ");
-            System.out.println("| ============================= |");
-            System.out.println("|                               |");
-            System.out.println("| ===== Digite uma opção ====== |");
-            System.out.println("| ===== 1: Cadastrar Novo ===== |");
-            System.out.println("| ==== 2: Listar Clientes ===== |");
-            System.out.println("| ==== 3: Procurar Cliente ==== |");
-            System.out.println("| ========= 0: Voltar ========= |");
-            escolha = s.nextInt();
-
-            switch (escolha){
-
-
-                case 1:
-
-                    controle.cadastrarNovoCliente();
-                    break;
-
-                case 2:
-
-                    controle.listarClientes();
-                    break;
-
-                case 3:
-                    controle.procurarCliente();
-                    break;
-
-                default:
-                    System.out.println("| ===== Opção invalida! ===== |");
-                break;
-            }
-
-        }while(escolha != 0);
 
     }
 }
