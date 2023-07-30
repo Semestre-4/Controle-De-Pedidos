f9ee6755c356d265d9751703d42b9deefcf326af
Bouchra Akl <bushraakl1234@gmail.com>
2023-07-30 17:07:52 -0300

    Enum Produto.java implemented.

--- src/main/java/org/desafio/Produto.java
+++ src/main/java/org/desafio/Produto.java
@@ -1,13 +1,28 @@
 package org.desafio;
 
 public enum Produto {
+    MARGHERITA("Margherita", 25.0),
+    CALABRESA("Calabresa", 28.5),
+    QUATRO_QUEIJOS("Quatro Queijos", 30.0),
+    PORTUGUESA("Portuguesa", 27.0),
+    FRANGO_CATUPIRY("Frango com Catupiry", 32.0),
+    PEPPERONI("Pepperoni", 29.5),
+    VEGETARIANA("Vegetariana", 26.0);
 
-    MARGHERITA,
-    CALABRESA,
-    QUATRO_QUEIJOS,
-    PORTUGUESA,
-    FRANGO_CATUPIRY,
-    PEPPERONI,
-    VEGETARIANA,
+    private String nome;
+    private double preco;
+
+    Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
 
 }
