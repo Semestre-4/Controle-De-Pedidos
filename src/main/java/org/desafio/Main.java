f1011834d86d3fcb34de4601cff0d83de2a39e0f
Bouchra Akl <bushraakl1234@gmail.com>
2023-07-30 17:07:17 -0300

    Main.main optimized based implemented.

--- src/main/java/org/desafio/Main.java
+++ src/main/java/org/desafio/Main.java
@@ -1,20 +1,41 @@
 package org.desafio;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
 
         ControleClientes controleClientes = new ControleClientes();
-        Scanner s = new Scanner(System.in);
+        ControlePedidos controlePedidos = new ControlePedidos(controleClientes);
 
-        List<Clientes> clientes = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
 
+        int escolha;
+        do {
+            // Exibe o menu principal
+            exibirMenu();
+            escolha = scanner.nextInt();
 
-        controleClientes.menuClientes();
+            switch (escolha) {
+                case 1 -> controleClientes.start(); // Executa o controle de clientes
+                case 2 -> controlePedidos.start(); // Executa o controle de pedidos
+                case 0 -> System.out.println("Saindo do programa...");
+                default -> System.out.println("Opção inválida. Por favor, tente novamente.");
+            }
+        } while (escolha != 0);
 
+        scanner.close();
+    }
 
+    // Método para exibir o menu principal
+    private static void exibirMenu() {
+        System.out.println();
+        System.out.println("====== Menu Principal ======");
+        System.out.println("1. Controle de Clientes");
+        System.out.println("2. Controle de Pedidos");
+        System.out.println("0. Sair");
+        System.out.println("=================================");
+        System.out.print("Digite a opção desejada: ");
     }
 }
